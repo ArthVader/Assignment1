175e719dbcfa34117760906cebdc27305327fe95
ArthVader <11413805+ArthVader@users.noreply.github.com>
2019-02-03 16:34:48 -0500

    Updated RoleGame.java
    Modified and completed the main method of the prototype to add new test
    cases for the Weapons/Classes that were added. These include SimpleHammer,
    CrazyRandomSword, and DemonBlade. Simulated the interation with a
    character with no armor, and character with 29 armor points.

--- src/edu/fsu/cs/cen5035/RoleGame.java
+++ src/edu/fsu/cs/cen5035/RoleGame.java
@@ -2,23 +2,35 @@ package edu.fsu.cs.cen5035;
 
 /**
  *
- * @author Javier
+ * @author Javier and Arthur Karapateas(Additional weapons and 2 test cases)
  */
 public class RoleGame {
 
     public static void main(String[] args) {
 
-    	int armor = 29;
+        //Testing weapons against a character with no armor, and a character with 29 armor points
+        for (int armor = 0; armor <= 29; armor += 29)
+    	{
+            Weapon weapon = WeaponFactory.getWeapon("sword");
+            System.out.println("Sword has " + weapon.hit() + " of damage.");        
+            System.out.println("Swords was able to do " + weapon.hit(armor) + " of damage due to an armor with "+armor+ " points.");
 
-        Weapon weapon = WeaponFactory.getWeapon("sword");
-        System.out.println("Sword has " + weapon.hit() + " of damage.");        
-        System.out.println("Swords was able to do " + weapon.hit(armor) + " of damage due to an armor with "+armor+ " points.");
+            weapon = WeaponFactory.getWeapon("spear");
+            System.out.println("Spear has " + weapon.hit() + " of damage.");        
+            System.out.println("Spear was able to do " + weapon.hit(armor) + " of damage due to an armor with "+armor+ " points.");
+            
+            //Additional weapons added here:
+            weapon = WeaponFactory.getWeapon("simpleHammer");
+            System.out.println("SimpleHammer has " + weapon.hit() + " of damage.");        
+            System.out.println("SimpleHammer was able to do " + weapon.hit(armor) + " of damage due to an armor with "+armor+ " points.");
 
-        weapon = WeaponFactory.getWeapon("spear");
-        System.out.println("Spear has " + weapon.hit() + " of damage.");        
-        System.out.println("Spear was able to do " + weapon.hit(armor) + " of damage due to an armor with "+armor+ " points.");
-        
-        //TODO: Add the remainig weapons here
+            weapon = WeaponFactory.getWeapon("crazyRandomSword");
+            System.out.println("CrazyRandomSword has " + weapon.hit() + " of damage.");        
+            System.out.println("CrazyRandomSword was able to do " + weapon.hit(armor) + " of damage due to an armor with "+armor+ " points.");
 
+            weapon = WeaponFactory.getWeapon("demonBlade");
+            System.out.println("DemonBlade has " + weapon.hit() + " of damage.");        
+            System.out.println("DemonBlade was able to do " + weapon.hit(armor) + " of damage due to an armor with "+armor+ " points.");
+        }
     }
 }
